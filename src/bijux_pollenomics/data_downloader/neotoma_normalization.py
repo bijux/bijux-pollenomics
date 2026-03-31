93d9cb022af8dd94686f6c02d121921986dc792e
bijan <mousavi.bijan@gmail.com>
2026-03-31 21:27:52 +0200

    refactor(src): remove unused compatibility re-export modules

--- src/bijux_pollenomics/data_downloader/neotoma_normalization.py
+++
@@ -1,2 +0,0 @@
-from .sources.neotoma.normalization import *  # noqa: F403
-from .sources.neotoma.normalization import __all__ as __all__
--- src/bijux_pollenomics/reporting/countries.py
+++
@@ -1,2 +0,0 @@
-from .bundles.country_selection import *  # noqa: F403
-from .bundles.country_selection import __all__ as __all__
