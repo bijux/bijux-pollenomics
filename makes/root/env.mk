4fa99560ae0703a57799f3cfbcefc719ea2f9f29
bijan <mousavi.bijan@gmail.com>
2026-04-08 23:17:38 +0200

    refactor: standardize root artifacts environment layout

--- makes/root/env.mk
+++ makes/root/env.mk
@@ -2,7 +2,11 @@ include $(ROOT_MAKEFILE_DIR)/bijux-py/root-env.mk
 
 .DEFAULT_GOAL := help
 ARTIFACTS_ROOT ?= artifacts
-VENV ?= $(ARTIFACTS_ROOT)/.venv
+PROJECT_DIR ?= $(CURDIR)
+PROJECT_SLUG ?= bijux-pollenomics
+PROJECT_ARTIFACTS_DIR ?= $(ARTIFACTS_ROOT)
+CONFIG_DIR ?= configs
+VENV ?= $(ROOT_VENV)
 BIN := $(VENV)/bin
 VENV_PYTHON := $(BIN)/python
 ACT := $(BIN)
@@ -12,14 +16,6 @@ ROOT_PACKAGE_SRC_DIR := $(ROOT_PACKAGE_DIR)/src
 ROOT_PACKAGE_TEST_DIR := $(ROOT_PACKAGE_DIR)/tests
 ROOT_DEV_PACKAGE_DIR := packages/bijux-pollenomics-dev
 ROOT_DEV_SRC_DIR := $(ROOT_DEV_PACKAGE_DIR)/src
-ROOT_ARTIFACTS_DIR := $(ARTIFACTS_ROOT)/root
-PROJECT_DIR ?= $(CURDIR)
-PROJECT_SLUG ?= bijux-pollenomics
-PROJECT_ARTIFACTS_DIR ?= $(ARTIFACTS_ROOT)
-CONFIG_DIR ?= configs
 MKDOCS_CFG ?= $(PROJECT_DIR)/mkdocs.yml
 ROOT_PYTHONPATH := $(abspath $(ROOT_PACKAGE_DIR)):$(abspath $(ROOT_PACKAGE_SRC_DIR)):$(abspath $(ROOT_DEV_SRC_DIR))
 UV_SYNC := UV_PROJECT_ENVIRONMENT=$(VENV) $(UV) sync --frozen --group dev --python $(PYTHON)
-
-export PYTHONDONTWRITEBYTECODE := 1
-export PYTHONPYCACHEPREFIX := $(ROOT_ARTIFACTS_DIR)/pycache
