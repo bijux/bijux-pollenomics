3bf0bace2fa0962c4d289ddbad86b21d1d7c2fb2
Bijan Mousavi <mousavi.bijan@gmail.com>
2026-04-27 23:09:49 +0200

    feat(reporting): add atlas bundle manifests

--- packages/bijux-pollenomics/src/bijux_pollenomics/reporting/bundles/summary_builders/__init__.py
+++ packages/bijux-pollenomics/src/bijux_pollenomics/reporting/bundles/summary_builders/__init__.py
@@ -1,9 +1,11 @@
+from .atlas import build_multi_country_bundle_manifest
 from .atlas import build_multi_country_map_summary
 from .country import build_country_bundle_manifest
 from .country import build_country_report_summary
 from .published import build_published_reports_summary
 
 __all__ = [
+    "build_multi_country_bundle_manifest",
     "build_country_bundle_manifest",
     "build_country_report_summary",
     "build_multi_country_map_summary",
