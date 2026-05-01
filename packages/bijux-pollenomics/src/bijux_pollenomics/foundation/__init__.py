4960e169083b9fb19d721248b14896c43460f3a1
Bijan Mousavi <mousavi.bijan@gmail.com>
2026-05-01 11:31:33 +0200

    test(alias): enforce pollenomics compatibility alias parity

--- packages/bijux-pollenomics/src/bijux_pollenomics/foundation/__init__.py
+++ packages/bijux-pollenomics/src/bijux_pollenomics/foundation/__init__.py
@@ -1,3 +1,9 @@
+from .alias import CompatibilityAliasContract, compatibility_alias_contract
 from .contracts import RuntimeSurfaceContract, runtime_surface_contract
 
-__all__ = ["RuntimeSurfaceContract", "runtime_surface_contract"]
+__all__ = [
+    "CompatibilityAliasContract",
+    "RuntimeSurfaceContract",
+    "compatibility_alias_contract",
+    "runtime_surface_contract",
+]
